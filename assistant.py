7896ef5fe3880b2d339e09a5208173c17f63356b
Deepansh05dk <84671261+Deepansh05dk@users.noreply.github.com>
2024-02-01 21:06:44 +0530

    adding instructions at run time

--- assistant.py
+++ assistant.py
@@ -2,6 +2,7 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import time
 load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI"))
@@ -50,7 +51,46 @@ client = OpenAI(api_key=os.getenv("OPENAI"))
 
 # client.beta.assistants.update(assistant_id="asst_7aMJYGOFx8Li3qDwc9OTQZ3y",instructions='You are a tweet generator on behalf of the @jackjayio account. I am giving it to you as a JSON file with an array of tweet objects. Each tweet object has a structure like this:{"text" : ...,"likes": ...,"retweets": ...,}.Now, taking into account more likes, retweets, and tweet text, copy the nuance of the user writing style from each tweet text so that you can generate a tweet on a specific topic in the same style.')
 
+# def polling_for_run_status(run,thread):
+#     while True:
+#         # Retrieve the run status
+#         run_status = client.beta.threads.runs.retrieve(
+#             thread_id=thread.id,
+#             run_id=run.id
+#         )
 
+#         # Check and print the step details
+#         run_steps = client.beta.threads.runs.steps.list(
+#             thread_id=thread.id,
+#             run_id=run.id
+#         )
+#         for step in run_steps.data:
+#             if step.type == 'tool_calls':
+#                 print(f"Tool {step.type} invoked.")
+
+#             # If step involves code execution, print the code
+#             if step.type == 'code_interpreter':
+#                 print(
+#                     f"Python Code Executed: {step.step_details['code_interpreter']['input']}")
+
+#         if run_status.status == 'completed':
+#             # Retrieve all messages from the thread
+#             messages = client.beta.threads.messages.list(
+#                 thread_id=thread.id
+#             )
+
+#             # Print all messages from the thread
+#             for msg in messages.data:
+#                 role = msg.role
+#                 content = msg.content[0].text.value
+#                 print(f"{role.capitalize()}: {content}")
+#             break  # Exit the polling loop since the run is complete
+#         elif run_status.status in ['queued', 'in_progress']:
+#             print(f'{run_status.status.capitalize()}... Please wait.')
+#             time.sleep(1.5)  # Wait before checking again
+#         else:
+#             print(f"Run status: {run_status.status}")
+#             break  # Exit the polling loop if the status is neither 'in_progress' nor 'completed'
 
 
 # thread = client.beta.threads.create()
@@ -62,9 +102,12 @@ client = OpenAI(api_key=os.getenv("OPENAI"))
 #     client.beta.threads.messages.create(
 #                 thread_id=thread.id,
 #                 role="user",
-#                 content="hello"
+#                 content=a
 #             )
 #     run = client.beta.threads.runs.create(
 #         thread_id=thread.id,
 #         assistant_id="asst_7aMJYGOFx8Li3qDwc9OTQZ3y"
 #     )
+#     polling_for_run_status(run=run,thread=thread)
+
+
--- main.py
+++ main.py
@@ -8,8 +8,13 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI"))
 file_id = 'file-tWDjaLWYr85RtjaF8YVSJzhP'
 assistant_id = "asst_7aMJYGOFx8Li3qDwc9OTQZ3y"
-thread_id = "thread_2yHRETtOdQf3cX1CawZI9wAO"
+thread_id = "thread_vxrs0Ew3yu1CFH3aT8dTvwKe"
 
+# client.beta.threads.messages.create(
+#     thread_id=thread_id,
+#     role="system",
+#     content='You are a tweet generator on behalf of the @jackjayio account. I am giving it to you as a JSON file with an array of tweet objects. Each tweet object has a structure like this:{"text" : ...,"likes": ...,"retweets": ...,}.Now, taking into account more likes, retweets, and tweet text, copy the nuance of the user writing style from each tweet text so that you can generate a tweet on a specific topic in the same style.'
+# )
 
 def polling_for_run_status(run):
     while True:
@@ -64,7 +69,7 @@ def create_new_message(prompt):
         run = client.beta.threads.runs.create(
             thread_id=thread_id,
             assistant_id=assistant_id,
-        )
+            instructions='You are a tweet generator on behalf of the @jackjayio account. I am giving it to you as a JSON file with an array of tweet objects. Each tweet object has a structure like this:{"text" : ...,"likes": ...,"retweets": ...,}.Now, taking into account more likes, retweets, and tweet text, copy the nuance of the user writing style from each tweet text so that you can generate a tweet on a specific topic in the same style.')
         polling_for_run_status(run=run)
 
     except Exception as e:
